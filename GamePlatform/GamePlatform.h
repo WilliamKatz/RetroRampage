8b69935052e90798100d8fcc6bf25a6cdb9441a2
Billy <bkatz>
2020-03-17 16:03:59 -0700

    Created project and internal framework

---
+++ GamePlatform/GamePlatform.h
@@ -0,0 +1,19 @@
+//
+//  GamePlatform.h
+//  GamePlatform
+//
+//  Created by Katz, Billy on 3/17/20.
+//  Copyright © 2020 KillyBatz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GamePlatform.
+FOUNDATION_EXPORT double GamePlatformVersionNumber;
+
+//! Project version string for GamePlatform.
+FOUNDATION_EXPORT const unsigned char GamePlatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GamePlatform/PublicHeader.h>
+
+
